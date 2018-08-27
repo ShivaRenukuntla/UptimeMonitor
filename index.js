79213074c77ad7aa7a3014de6a7e13e72bddb846
ShivaRenukuntla <shivasurya.prasad4@gmail.com>
2018-08-27 19:49:12 +0530

    Getting the url and parse it, Get the path from the url Send the response Log the request

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var http = require("http");
+var url = require("url");
 
 var server = http.createServer(function(req, res) {
+  //get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+  //get the path from the url
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  //send the response
   res.end("Server is initiated and Hello World");
+  //log what path does the person ius asking for i.e. log the request
+  console.log("Request is received on this path: " + trimmedPath);
 });
 
 server.listen(3000, function() {
